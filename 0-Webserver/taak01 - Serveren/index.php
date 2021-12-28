badc3b9b06462097ff98d73951d5112da769d07a
Yahya Ben Abdessadak <2065118@talnet.nl>
2021-12-28 15:39:16 +0100

    Added null adn true that made 1.

--- 0-Webserver/taak01 - Serveren/index.php
+++ 0-Webserver/taak01 - Serveren/index.php
@@ -2,8 +2,14 @@
     $Leeftijd = 21;
     $Melk = 1.75;
     $MijnLeeftijd = "Yahya is $Leeftijd jaar <br>";
-    $MelkPrijs = "Melk kost $Melk";
+    $MelkPrijs = "Melk kost $Melk <br>";
 
     echo $MijnLeeftijd;
     echo $MelkPrijs;
+
+    $True = true;
+    $Null = null;
+
+    echo $True;
+    echo $Null;
 ?>
