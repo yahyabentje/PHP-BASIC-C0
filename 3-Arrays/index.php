337a8802ff53b6f78310cf0abccde49431465b2e
Yahya Ben Abdessadak <2065118@talnet.nl>
2022-01-12 07:21:20 +0100

    Coded the arays with valid code of the index.

--- 3-Arrays/index.php
+++ 3-Arrays/index.php
@@ -4,6 +4,20 @@ $myMovies = ["Matrix1, Matrix2, Matrix3."];
 
 echo $myMovies[0];
 
+$myDogsNames = ["Parel, Zelda, Nocus"];
+
+echo "<br>";
+
+echo "My dogs can have 3 names";
+
+echo $myDogsNames[0];
+
+echo "<br>";
+
+$myDogsNames[0] = "parel";
+
+echo $myDogsNames[0];
+
 ?>
 
 
